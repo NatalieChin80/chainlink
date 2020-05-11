75c8a05f959c1566368ab2a00183bacb0fccd459
Sam Davies <sam@samueldavies.net>
2020-05-11 16:36:23 +0100

    Set up eth_transmissions and tables

---
+++ core/store/migrations/migration1589206996/migrate.go
@@ -0,0 +1,66 @@
+package migration1589206996
+
+import (
+	"github.com/jinzhu/gorm"
+)
+
+func Migrate(tx *gorm.DB) error {
+	return tx.Exec(`
+	  	CREATE TABLE eth_transactions (
+			id BIGSERIAL PRIMARY KEY,
+			nonce bigint NOT NULL,
+			from_address bytea NOT NULL,
+			to_address bytea NOT NULL,
+			encoded_payload bytea NOT NULL,
+			value numeric(78, 0) NOT NULL,
+			created_at timestamptz NOT NULL
+	  	);
+
+		CREATE UNIQUE INDEX idx_eth_transactions_nonce_from_address ON eth_transactions (nonce, from_address);
+		CREATE INDEX idx_eth_transactions_created_at ON eth_transactions USING BRIN (created_at);
+
+		CREATE TABLE eth_transaction_attempts (
+			id BIGSERIAL PRIMARY KEY,
+		 	eth_transaction_id bigint REFERENCES eth_transactions (id) NOT NULL,
+		 	gas_price numeric(78,0) NOT NULL,
+		 	gas_limit bigint NOT NULL,
+		 	signed_raw_tx bytea NOT NULL,
+		 	hash bytea,
+		 	error text,
+		 	confirmed_in_block_num bigint,
+		 	confirmed_in_block_hash bytea,
+		 	confirmed_at timestamptz,
+		 	created_at timestamptz NOT NULL
+		);
+
+		ALTER TABLE eth_transaction_attempts ADD CONSTRAINT chk_cannot_have_both_hash_and_error CHECK (
+			(error IS NULL AND hash IS NULL AND confirmed_in_block_num IS NULL AND confirmed_in_block_hash IS NULL AND confirmed_at IS NULL)
+			OR
+			(error IS NOT NULL AND hash IS NULL AND confirmed_in_block_num IS NULL AND confirmed_in_block_hash IS NULL AND confirmed_at IS NULL)
+			OR
+			(error IS NULL AND hash IS NOT NULL AND confirmed_in_block_num IS NOT NULL AND confirmed_in_block_hash IS NOT NULL AND confirmed_at IS NOT NULL)
+		);
+
+		CREATE UNIQUE INDEX idx_eth_transaction_attempts_signed_raw_tx ON eth_transaction_attempts (signed_raw_tx);
+		CREATE UNIQUE INDEX idx_eth_transaction_attempts_hash ON eth_transaction_attempts (hash);
+		CREATE UNIQUE INDEX idx_eth_transaction_attempts_only_one_finalized ON eth_transaction_attempts (eth_transaction_id) WHERE (error IS NOT NULL) OR (hash IS NOT NULL);
+		CREATE INDEX idx_eth_transaction_attempts ON eth_transaction_attempts (eth_transaction_id);
+		CREATE INDEX idx_eth_transactions_confirmed_in_block_num ON eth_transaction_attempts (confirmed_in_block_num) WHERE confirmed_in_block_num IS NOT NULL;
+		CREATE INDEX idx_eth_transactions_attempts_created_at ON eth_transaction_attempts USING BRIN (created_at);
+
+		CREATE TABLE eth_transmissions (
+			id BIGSERIAL PRIMARY KEY,
+			task_run_id uuid NOT NULL REFERENCES task_runs (id) ON DELETE CASCADE,
+			eth_transaction_id bigint REFERENCES eth_transactions (id) ON DELETE CASCADE,
+			from_address bytea NOT NULL,
+			to_address bytea NOT NULL,
+			encoded_payload bytea NOT NULL,
+			created_at timestamptz NOT NULL
+		);
+
+		CREATE UNIQUE INDEX idx_eth_transmissions_task_run_id ON eth_transmissions (task_run_id);
+		CREATE UNIQUE INDEX idx_eth_transmissions_eth_transaction_id ON eth_transmissions (eth_transaction_id);
+		CREATE INDEX idx_eth_transmissions_created_at ON eth_transmissions USING BRIN (created_at);
+
+	`).Error
+}
